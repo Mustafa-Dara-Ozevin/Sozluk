eeace498c9dda1f7087bf97c3603cd68742134a0
Mustafa-Dara-Ozevin <mustafadara.ozevin@outlook.com>
2023-04-27 13:55:57 +0200

    sozluk

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,60 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use core::fmt;
+
+pub struct Word {
+    pub name: String,
+    pub definitions: Vec<String>,
+    pub def_count: usize,
+}
+
+impl fmt::Display for Word {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
+
+impl Word {
+    pub fn new() -> Word {
+        Word {
+            name: String::new(),
+            definitions: Vec::new(),
+            def_count: 0,
+        }
+    }
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+}
+#[tauri::command]
+fn search(query: &str) -> Vec<String> {
+    let mut word = Word::new();
+    let url = format!(
+        "https://sozluk.gov.tr/gts?ara={}",
+        query.trim().to_lowercase()
+    );
+    let res = reqwest::blocking::get(url).unwrap();
+
+    let body = res.json::<serde_json::Value>().unwrap();
+    let dif_defs = body.to_string().matches("anlamlarListe").count();
+    for j in 0..dif_defs {
+        let mut definition_count: String = body[0]["anlam_say"].to_string(); // can't parse the string because it is ""number"" instead "number"
+        definition_count.retain(|c| c != '"'); // use retain to remove extra double quotes
+        word.def_count = definition_count.parse().unwrap();
+        for i in 0..word.def_count {
+            let meaning = body[j]["anlamlarListe"][i]["anlam"].to_string().replace("\"", "");
+            if meaning != "null" {
+                word.definitions.push(meaning);
+            }
+        }
+    }
+    return word.definitions
+}
+
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![search])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/main.js
@@ -0,0 +1,40 @@
+const { invoke } = window.__TAURI__.tauri;
+
+let greetInputEl;
+let meaningsEl;
+
+async function search() {
+  
+  const meanings = await invoke("search", { query: greetInputEl.value });
+  let meaning = "";
+  meaningsEl.textContent = '';
+  if (meanings.length === 0){
+    meaningsEl.textContent = "Kelime bulunamadı!"
+  }else{
+    for (meaning of meanings){
+      const li = document.createElement("li");
+      li.textContent = meaning;
+      meaningsEl.appendChild(li);
+    }
+
+  }
+  
+  
+}
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#search-input");
+  meaningsEl = document.querySelector("#meanings");
+
+  document
+    .querySelector("#search-button")
+    .addEventListener("click", () => search());
+
+  /*
+  document
+  .querySelector("#search-input")
+  .addEventListener("keydown", () => search())
+  */
+});
